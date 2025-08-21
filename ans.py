082a729c1bce69232ef09a117c5e1c5b289616d5
SuzzyM <suwanista@gmail.com>
2025-08-21 22:03:05 +0300

    Add files via upload

---
+++ ans.py
@@ -0,0 +1,15 @@
+def calculate_discount(price, discount_percent):
+    discount_percent = 20/100
+    price = price - (20/100)
+    print(price - discount_percent)
+
+    price = 1200
+    discount_percent = 20/100
+print(1200 - (20/100))
+
+
+
+
+
+
+        
